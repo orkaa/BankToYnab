6f2305ef6497b3aed7ffb9b3faba64fbbfa6169d
Nace Oroz <nace@weld.app>
2025-07-12 22:08:19 +0200

    refactor: Relocate test files to be alongside components and remove custom Jest config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Bank CSV to YNAB Converter heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Bank CSV to YNAB Converter/i);
+  expect(headingElement).toBeInTheDocument();
 });
